6e3d75845055938f6a375b52398a9fc22414fe7c
xjwhust <45335837+xjwhust@users.noreply.github.com>
2021-03-04 12:26:32 +0800

    Update qmydCOOKIE.js

--- Task/qmydCOOKIE.js
+++ Task/qmydCOOKIE.js
@@ -1,6 +1,6 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let qmydtokenVal = ``
+let qmydtokenVal = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcW15ZC55aWNoZW5ndy5jblwvYXBwc1wvbG9naW5cL3dlY2hhdCIsImlhdCI6MTYxNDgzMTgyNiwiZXhwIjoyMDUzNjQzMTgyNiwibmJmIjoxNjE0ODMxODI2LCJqdGkiOiJPQW5UcFpLS3VwaFU0UFQ2Iiwic3ViIjoxMTYyNjksInBydiI6IjQxZGY4ODM0ZjFiOThmNzBlZmE2MGFhZWRlZjQyMzQxMzcwMDY5MGMifQ.wPEMezJZWlZkF7JZ4Iyj1GOfL4HhyrTDT4MNzjRDOq4`
 
 
 
