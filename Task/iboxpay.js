bb658d61c9dbce46215e092e415f2080b9289efb
xjwhust <45335837+xjwhust@users.noreply.github.com>
2021-03-07 15:40:09 +0800

    Update iboxpay.js

--- Task/iboxpay.js
+++ Task/iboxpay.js
@@ -1,8 +1,8 @@
 /* ziye 
-github地址 https://github.com/ziye12
+github地址 https://github.com/6Svip120apk69
 TG频道地址  https://t.me/ziyescript
 TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
-boxjs链接  https://raw.githubusercontent.com/ziye12/JavaScript/main/Task/ziye.boxjs.json
+boxjs链接  https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/ziye.boxjs.json
 转载请备注个名字，谢谢
 
 ⚠️笑谱
@@ -40,6 +40,8 @@ boxjs链接  https://raw.githubusercontent.com/ziye12/JavaScript/main/Task/ziye.
 2.10 修复红包雨问题，LIVE设置3  启动红包雨活动，修复版本问题
 2.10-2 移除红包雨模块
 2.11 移除视频时间限制，LIVE设置666做新人180秒任务
+2.26 适配直播上限20次
+3.5 设置888由上至下循环提现
 
 ⚠️一共1个位置 1个ck  👉 5条 Secrets 
 多账号换行
@@ -72,8 +74,8 @@ refreshtokenVal 👉XP_refreshTOKEN
 设置任务 可设置 0 1 2    0开视频关直播 1开视频开直播 2关视频开直播
  LIVE  👉  XP_live
 
-设置提现金额 可设置 0 1 15 30 50 100  默认0关闭
-CASH  👉  XP_CASH
+设置提现金额 可设置 0 1 15 30 50 100 888 默认0关闭  设置888由上至下循环提现
+CASH  👉  XP_CASH 
 
 设置手机号 
  phone  👉  XP_phone
@@ -90,15 +92,15 @@ hostname=veishop.iboxpay.com
 ############## 圈x
 
 #笑谱获取更新TOKEN
-https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/* url script-response-body https://raw.githubusercontent.com/ziye12/JavaScript/main/Task/iboxpay.js
+https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/* url script-response-body https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/iboxpay.js
 
 ############## loon
-http-response https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/* script-path=https://raw.githubusercontent.com/ziye12/JavaScript/main/Task/iboxpay.js, requires-body=1,max-size=0, tag=笑普token
+http-response https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/* script-path=https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/iboxpay.js, requires-body=1,max-size=0, tag=笑普token
 
 ############## surge
 
 #笑谱获取更新TOKEN
-笑谱获取更新TOKEN = type=http-response,pattern=https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/*,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ziye12/JavaScript/main/Task/iboxpay.js
+笑谱获取更新TOKEN = type=http-response,pattern=https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ignore_tk\/veishop\/v1\/*,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/iboxpay.js
 
 
 
@@ -274,7 +276,39 @@ function time(inputTime) {
     s = date.getSeconds();
     return Y + M + D + h + m + s;
 };
-
+//随机udid 大写
+function udid() {
+    var s = [];
+    var hexDigits = "0123456789ABCDEF";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+    var uuid = s.join("");
+    return uuid;
+}
+//随机udid 小写
+function udid2() {
+    function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+//编码
+function encodeUnicode(str) {
+    var res = [];
+    for (var i = 0; i < str.length; i++) {
+        res[i] = ("00" + str.charCodeAt(i).toString(16)).slice(-4);
+    }
+    return "\\u" + res.join("\\u");
+}
+//解码
+function decodeUnicode(str) {
+    str = str.replace(/\\/g, "%");
+    return unescape(str);
+}
 
 
 let isGetCookie = typeof $request !== 'undefined'
@@ -336,11 +370,30 @@ async function all() {
         await splimit(); //视频上限
         await newcashlist(); //提现查询
         await cashlist(); //今日提现查询
-        if (!cashcs.amount && CASH >= 1 && $.coin.data.balance / 100 >= CASH) {
+        if (!cashcs.amount && CASH >= 1 && CASH <= 100 && $.coin.data.balance / 100 >= CASH) {
             await withdraw(); //提现
         }
 
-        if (LIVE >= 1 && nowTimes.getHours() >= 8 && nowTimes.getHours() <= 23 && $.sylist.resultCode && livecs < 30) {
+        if (!cashcs.amount && CASH == 888) {
+
+            if ($.coin.data.balance / 100 >= 100) {
+                CASH = 100
+            } else if ($.coin.data.balance / 100 >= 50) {
+                CASH = 50
+            } else if ($.coin.data.balance / 100 >= 30) {
+                CASH = 30
+            } else if ($.coin.data.balance / 100 >= 15) {
+                CASH = 15
+            } else if ($.coin.data.balance / 100 >= 1) {
+                CASH = 1
+            }
+
+            if (CASH != 888) {
+                await withdraw(); //提现
+            }
+        }
+
+        if (LIVE >= 1 && nowTimes.getHours() >= 8 && nowTimes.getHours() <= 23 && $.sylist.resultCode && livecs < 20) {
             await liveslist(); //直播节目表
             if (liveIdcd >= 1) {
                 dd = liveIdcd * 35 - 34
@@ -351,7 +404,7 @@ async function all() {
             }
         }
 
-        if ( LIVE != 2 && $.splimit.data.isUperLimit == false || LIVE == 888) {
+        if (LIVE != 2 && $.splimit.data.isUperLimit == false || LIVE == 888) {
 
             await playo(); //播放o       
             await videoo(); //视频o
@@ -1308,6 +1361,7 @@ function cashlist(timeout = 0) {
                     if (logs) $.log(`${O}, 今日提现记录🚩: ${data}`);
                     $.cashlist = JSON.parse(data);
                     if ($.cashlist.resultCode == 1 && data.match(/提现/g)) {
+
                         cashcs = $.cashlist.data.records.find(item => item.tradeTypeName === "提现")
                         console.log('今日已提现' + cashcs.amount / 100 + '元\n')
                         $.message +=
